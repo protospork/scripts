b1df42558c14809243bfec0e74e98639e3294e40
Scott Curfman <protospork@gmail.com>
2011-10-29 01:04:17 -0400

    added prompt for album name if undefined. closes GH-24

--- misc/imgur.pl
+++ misc/imgur.pl
@@ -3,28 +3,36 @@ use LWP;
 use LWP::Simple;
 use File::Path qw(make_path);
 use HTML::TreeBuilder;
-#imgur album rip script
+#Yes, this script uses both LWP and LWP::Simple. No, I am not sorry.
 
 #todo: can't trigger pages past 1
 
-my $album = $ARGV[0] || die "give it a URL";
+my $wingit = $#ARGV; #if you launch the script with -q (or -anythingelse <_<) it won't prompt for album names
+my $album = $ARGV[-1] || die "give it a URL";
 
+#"properly" $album should be a URI object
 if ($album =~ m{/a/}){
-	$album =~ s{/\d$|#\w+$}{};	#remove page numbers to get down to the root of the album
+	$album =~ s{/\d$|#\w+$}{};	#remove anchors to get down to the root of the album
 	$album .= "/all" unless $album =~ m{/all$}i;	#now go back to the index page
 }
 say $album;
 my $page = LWP::UserAgent->new()->get($album) or die "$!";
 die $page->status_line unless $page->is_success;
 
+#why did I use treebuilder for so little?
 my $albumname = HTML::TreeBuilder->new_from_content($page->decoded_content)->look_down(_tag => 'title')->as_text;
 $albumname =~ s/^\s*(.+?) - Imgur.*$/$1/;
 
-if ($albumname =~ /^(Photo Albums|Album)$/){
-	$albumname = $album;
-	$albumname =~ s{^.+com/(?:a/)?([^\s/]+)(?:/all)?}{$1}i;	
+if ($albumname =~ /^(Photo Albums?|Album)$/){
+	if ($wingit){
+		$albumname = $album;
+		$albumname =~ s{^.+com/(?:a/)?([^\s/]+)(?:/all)?}{$1}i;	
+	} else {
+		say "Please name this album.";
+		$albumname = <STDIN>;
+		$albumname =~ s/\n$//;
+	}
 }
-#my @imagehashes = ($page =~ /http\S+\/([a-zA-Z0-9]{5,6})s\.jpg/ig);
 my @imagehashes = ($page->decoded_content =~ /<div id="([a-zA-Z0-9]{5})" class="post">/ig);
 say ((1 + $#imagehashes).' images');
 length $albumname > 120 ? die 'broken albumname parse' : say $albumname;
