fa859955d4449d63e0cf939572dc7ac758057580
Scott Curfman <protospork@gmail.com>
2011-12-24 00:29:05 -0500

    -added jedict support (perl kana.pl path/to/jedict) -added terminal colors

--- misc/kana.pl
+++ misc/kana.pl
@@ -1,45 +1,93 @@
 use Modern::Perl;
 use Text::Unidecode;
+use Term::ANSIColor;
 
 #a hiragana trainer... sort of flash card thingy
 #might add katakana later
+#todo: add the ability to slurp a list of runed words and shuffle output them
 
 
 binmode STDOUT, ":utf8"; #turn off that ridiculous widechar warning
 
-my @gana = (12353..12431, 12434, 12435);
-#these are the vowels and N, I think it makes sense to weight them a bit more
-push @gana, (12353, 12356, 12357, 12360, 12362, 12435); 
-#push @gana, (12432, 12433, 12436); #wi, we, and vu - but they're useless IRL
-map { $_ = chr $_ } @gana;
+$ARGV[0] ? take($ARGV[0]) : nonsense();
 
-print "Round Length? ";
-my $num = 0 + <STDIN>;
-say $num.' "words" then.';
+sub nonsense {
+	my @gana = (12353..12431, 12434, 12435);
+	#these are the vowels and N, I think it makes sense to weight them a bit more
+	push @gana, (12353, 12356, 12357, 12360, 12362, 12435); 
+	#push @gana, (12432, 12433, 12436); #wi, we, and vu - but they're useless IRL
+	map { $_ = chr $_ } @gana;
 
-my ($right, $wrong) = (0, 0);
-while ($num){
-	my ($string, $len) = ($gana[int rand $#gana], (int rand 5)+1);
-	while ($len){ $len--; $string .= $gana[int rand $#gana]; }
-	say $string;
-	chomp(my $in = <STDIN>);
-	
-	#extra space for single-char sounds helps keeping track of where you are
-	$in =~ s/ //g; 
-	#unidecode disagrees with my books on these
-	$in =~ s/shi/si/g;
-	$in =~ s/tsu/tu/g;
-	$in =~ s/chi/ti/g;
-	$in =~ s/fu/hu/g;
-	
-	if ($in ~~ lc(unidecode($string))){
-		$right++;
-		say 'yep ('.$right.' right|'.$wrong.' wrong)';
-	} else {
-		$wrong++;
-		say 'no, it\'s '.(unidecode $string).' ('.$right.' right|'.$wrong.' wrong)';
+	print colored ("Round Length? ", 'white');
+	my $num = 0 + <STDIN>;
+	say colored ($num.' "words" then.', 'green');
+
+	my ($right, $wrong) = (0, 0);
+	while ($num){
+		my ($string, $len) = ($gana[int rand $#gana], (int rand 5)+1);
+		while ($len){ $len--; $string .= $gana[int rand $#gana]; }
+		say colored ($string, 'cyan');
+		chomp(my $in = <STDIN>);
+		
+		#extra space for single-char sounds helps keeping track of where you are
+		$in =~ s/ //g; 
+		#unidecode disagrees with my books on these
+		$in =~ s/shi/si/g;
+		$in =~ s/tsu/tu/g;
+		$in =~ s/chi/ti/g;
+		$in =~ s/fu/hu/g;
+		
+		if ($in ~~ lc(unidecode($string))){
+			$right++;
+			say colored ('yep ('.$right.' right|'.$wrong.' wrong)', 'green');
+		} else {
+			$wrong++;
+			say colored ('no, it\'s '.(unidecode $string).' ('.$right.' right|'.$wrong.' wrong)', 'red');
+		}
+		$num--;
 	}
-	$num--;
+
+#	say 'RESULTS: '.$right.' correct, '.$wrong.' messed up.'; #redundant
 }
 
-say 'RESULTS: '.$right.' correct, '.$wrong.' messed up.';
+sub take {
+	open my $file, '<:encoding(euc-jp)', $_[0] || die $!;
+	my %entries;
+	while (<$file>){
+		my ($term, $def) = ($_ =~ m!^.+?\[([^;]+?)(?:;[^\]]+)*\]\s+/(.+?)(?:/\(2\).+)?/$!);
+#		say $term if defined $term; #slows down the load and is obviously spammy
+		next unless defined $term;
+		if ($term =~ /[^\p{Hiragana}]/ || $def =~ /\(obsc\)/){ next; }
+		else { $entries{$term} = $def; }
+	}
+	say ((scalar keys %entries).' words in dictionary.');
+	
+	print colored ("Round Length? ", 'white');
+	my $num = 0 + <STDIN>;
+	say colored ($num.' words then.', 'green');
+	
+	
+	my ($right, $wrong, @gana) = (0, 0, keys %entries);
+	while ($num){
+		my ($string) = ($gana[int rand $#gana]);
+		say colored ($string.' {'.$entries{$string}.'}', 'cyan');
+		chomp(my $in = <STDIN>);
+		
+		#extra space for single-char sounds helps keeping track of where you are
+		$in =~ s/ //g; 
+		#unidecode disagrees with my books on these
+		$in =~ s/shi/si/g;
+		$in =~ s/tsu/tu/g;
+		$in =~ s/chi/ti/g;
+		$in =~ s/fu/hu/g;
+		
+		if ($in ~~ lc(unidecode($string))){
+			$right++;
+			say colored ('yep ('.$right.' right|'.$wrong.' wrong)', 'green');
+		} else {
+			$wrong++;
+			say colored ('no, it\'s '.(unidecode $string).' ('.$right.' right|'.$wrong.' wrong)', 'red');
+		}
+		$num--;
+	}
+}
