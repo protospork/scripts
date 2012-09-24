4353267608f1c4200fae8672d1a4d2b2eb9b7007
Scott Curfman <protospork@gmail.com>
2012-09-24 17:55:57 -0400

    EXPANDURL: handle NYT's stupid paywall

--- misc/znc-expandurl.pm
+++ misc/znc-expandurl.pm
@@ -36,7 +36,13 @@ sub OnChanMsg {
 		}
 		my $orig_url;
 		for ($req->redirects){ #iterate over the redirect chain, but only keep the final one
-			$orig_url = $_->header('Location');
+			if ($_->header('Location') =~ m[nytimes.com/glogin]i){ 
+				#NYT paywall redirects you according to some arcane logic that changes every week.
+				#Upshot: the final hop is worthless and extremely long.
+				last;
+			} else {
+				$orig_url = $_->header('Location');
+			}
 		}
 		if (! $orig_url){
 			$self->PutModule("$url is actual url") if $debug;
@@ -44,13 +50,8 @@ sub OnChanMsg {
 		}
 		if (length $orig_url > 140){ 
 			$self->PutModule("$orig_url is too long: ".(length $orig_url)."ch") if $debug;
-			#-filter nyt glogin links to proper ones
-			$orig_url =~ s{^http://www.nytimes.com/glogin?URI=}{}i;
-			#-check length again
-			if (length $orig_url > 140){
-				#-strip all queries (they tend to help so I'd rather leave them if possible)
-				$orig_url =~ s{\?.+$}{};
-			}
+			#-strip all queries (they tend to help so I'd rather leave them if possible)
+			$orig_url =~ s{\?.+$}{};
 			#-check length again
 			if (length $orig_url > 140){ return $ZNC::CONTINUE; }
 		}
