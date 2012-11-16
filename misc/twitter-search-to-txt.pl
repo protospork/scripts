0b040d44f1dce57f82519527c8fc0a6e91da07c0
Scott Curfman <protospork@gmail.com>
2012-11-16 04:23:24 -0500

    this doesn't work anymore I guess

--- misc/twitter-search-to-txt.pl
+++ misc/twitter-search-to-txt.pl
@@ -7,26 +7,29 @@ use LWP;
 #grab twitter's search api response, maybe do some filtering, dump them in a text file
 #YOU WILL NEED TO EDIT THE FINAL LIST. Foreigners use twitter too. Also I never added any dupe detection.
 
-#the only line you need to change.
-my $mode = 0;
+#also what the fuck why did it stop working
+
+my $mode = $ARGV[0];
 
 my @modes = (
 	["#natesilverfacts", "#natesilverfacts -RT since=2012-11-06", "X:\\My Dropbox\\Public\\misc\\txt\\natesilver.txt"],
+	["#drunknatesilver", "#drunknatesilver -RT since=2012-11-06", "X:\\My Dropbox\\Public\\misc\\txt\\drunknatesilver.txt"],
 );
 
 my $url = "http://search.twitter.com/search.json?q=".uri_escape_utf8($modes[$mode][1]);
 
-my $req = LWP::UserAgent->new()->get($url);
+my $req = LWP::UserAgent->new(agent => 'Mozilla/5.0 (X11; U; Linux; i686; en-US; rv:1.9.0.13) Gecko/2009073022 Firefox/3.0.13')->get($url);
 die $req->status_line unless $req->is_success;
+
 
 my $mess = decode_json $req->decoded_content; #I think it's a hashref
 my $results = $mess->{'results'}; #but this should be an arrayref
 
-#say $results->[$_]{'text'} for (0..$#{$results});
-say $#{$results}." results";
-
 my (@facts, @todo);
 push @todo, $results->[$_]{'text'} for (0..$#{$results});
+
+say $_ for (0..$#todo);
+say $#todo." results";
 
 for (@todo){
 	s/$modes[$mode][0]//is; #strip hashtag
@@ -35,6 +38,7 @@ for (@todo){
 	s/(?<!\n)$/\n/;
 	
 	if (/https?:/){ #news stories are boring
+		print "Skipped: ".$_;
 		next;
 	} else {
 		push @facts, $_;
